43052539061b022ab34d1db1b26d58ea2d874f73
Edrian Aldrin Mariñas <edrian.a.marinas@gmail.com>
2026-02-07 13:00:55 +0800

    Setup main.py with FastAPI app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,8 @@
 from fastapi import FastAPI
-from routers import transactions, users, reports
+from app.routers import transactions, users, reports
 
 app = FastAPI()
 
 app.include_router(transactions.router)
-#app.include_router(users.router)
-#app.include_router(reports.router)
-
-
-@app.get("/")
-def read_root():
-  return {"Hello": "World"}
-
-
-#uvicorn app.main:app --reload
-#http://127.0.0.1:8000/
+app.include_router(users.router)
+app.include_router(reports.router)
