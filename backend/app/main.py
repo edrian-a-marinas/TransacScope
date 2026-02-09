d6bf1d1bf6b091ba6673ea445722d81d8c3d67be
Edrian Mariñas <edrian.a.marinas@gmail.com>
2026-02-09 08:29:09 +0800

    init pages front-end routing

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,19 @@ from app.routers import transactions, users, reports
 
 app = FastAPI()
 
+# 1. Mount Static Files (CSS, JS, Images)
+# This makes files in /static accessible via ://yourdomain.com
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# 2. Backend API Routings (Returns JSON)
 app.include_router(transactions.router)
 #app.include_router(users.router)
 #app.include_router(reports.router)
+
+# 3. Frontend Page Routings (Returns HTML)
+#app.include_router(what?.What? idk)
+
+
+
+
+
