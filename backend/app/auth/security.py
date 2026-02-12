bde4c7a73c26e094dde7ff4f4ec839bee1039ffb
Edrian Aldrin Mariñas <edrian.a.marinas@gmail.com>
2026-02-12 13:29:19 +0800

    JWT now working, log in and register working connected to DB

--- backend/app/auth/security.py
+++ backend/app/auth/security.py
@@ -1,20 +1,10 @@
-from passlib.context import CryptContext
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta
 import jwt
 from .config import JWT_SECRET_KEY, JWT_ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
-
-def hash_password(password: str) -> str:
-  return pwd_context.hash(password)
-
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-  return pwd_context.verify(plain_password, hashed_password)
-
 
 def create_access_token(data: dict):
   to_encode = data.copy()
@@ -22,7 +12,6 @@ def create_access_token(data: dict):
   to_encode.update({"exp": expire})
   encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
   return encoded_jwt
-
 
 def decode_access_token(token: str):
   try:
@@ -33,13 +22,15 @@ def decode_access_token(token: str):
   except jwt.InvalidTokenError:
     raise HTTPException(status_code=401, detail="Invalid token")
 
-
 async def get_current_user(token: str = Depends(oauth2_scheme)):
   payload = decode_access_token(token)
   user_id = payload.get("user_id")
   role_id = payload.get("role_id")
+
   if user_id is None or role_id is None:
     raise HTTPException(status_code=401, detail="Invalid authentication")
-  return {"id": user_id, "role_id": role_id}
 
-
+  return {
+    "id": user_id,
+    "role_id": role_id
+  }
