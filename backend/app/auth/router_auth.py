6bf57806a610c2873067bfad12ddc64ce0987b45
Edrian Aldrin Mariñas <edrian.a.marinas@gmail.com>
2026-02-21 11:23:52 +0800

    created /me endpoint for userID frontend saving

--- backend/app/auth/router_auth.py
+++ backend/app/auth/router_auth.py
@@ -9,30 +9,30 @@ router = APIRouter(prefix="/api/auth")
 
 @router.post("/register", response_model=UserRead)
 async def register_user(user: UserCreate):
-    new_user = await create_user(user)
-    return new_user
+  new_user = await create_user(user)
+  return new_user
 
 @router.post("/login")
 async def login_user(payload: UserLogin):
-    db_user = await verify_user(payload.email, payload.password)
-    if not db_user:
-        raise HTTPException(status_code=401, detail="Invalid credentials or inactive account")
+  db_user = await verify_user(payload.email, payload.password)
+  if not db_user:
+    raise HTTPException(status_code=401, detail="Invalid credentials or inactive account")
 
-    access_token = create_access_token({
-        "user_id": db_user["id"],
-        "role_id": db_user["role_id"]
-    })
+  access_token = create_access_token({
+    "user_id": db_user["id"],
+    "role_id": db_user["role_id"]
+  })
 
-    # Return the full user info along with token
-    return {
-        "access_token": access_token,
-        "token_type": "bearer",
-        "user": db_user  # <-- include user object
-    }
+  # Return the full user info along with token
+  return {
+    "access_token": access_token,
+    "token_type": "bearer",
+    "user": db_user  # <-- include user object
+  }
 
 @router.get("/me", response_model=UserRead)
 async def get_me(current_user=Depends(get_current_user)):
-    user = await get_user_by_id(current_user["id"])
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+  user = await get_user_by_id(current_user["id"])
+  if not user:
+    raise HTTPException(status_code=404, detail="User not found")
+  return user
