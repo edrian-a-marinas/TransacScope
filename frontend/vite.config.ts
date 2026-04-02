0ac3432264608473e246c11227c79e8e70fa64d5
Edrian Aldrin Mariñas <edrian.a.marinas@gmail.com>
2026-04-02 12:27:42 +0800

    Added a proxy: /api

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,5 +12,12 @@ export default defineConfig({
   server: {
     allowedHosts: ["transacscope.local"],
     port: 80,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+        ws: true,
+      },
+    },
   },
 });
